4d0b415956d74af4da6055e69008d8ee6dd38545
ZabrainAli <48334272+ZabrainAli@users.noreply.github.com>
2023-02-11 02:37:02 -0500

    Update test_GPSPreprocess.py
    Added test case for smoothData
    
    Co-Authored-By: Jasper Leung <22846809+JasperLeungGit@users.noreply.github.com>
    Co-Authored-By: Mike Li <58126566+mikeli0623@users.noreply.github.com>

--- src/tests/test_GPSPreprocess.py
+++ src/tests/test_GPSPreprocess.py
@@ -52,3 +52,31 @@ def test_filterData(sample_gps):
     assert Duplicate2[4] == True
     for i in Duplicate3:
         assert i == False
+
+@pytest.mark.parametrize(
+    'sample_gps', [(sample_gps_file_path+'/sample-gps-1.csv')]
+)
+def test_smoothData(sample_gps):
+    Data = pd.read_csv(filepath_or_buffer=sample_gps, nrows=4)
+    DataLength = len(Data)
+    GPSData = gpsp.GPSPreprocess(data=None)
+    GPSData.data = Data
+    SmoothData1 = GPSData.smoothData(GPSData.data)
+    SmoothData1Length = len(SmoothData1)
+
+    GPSData.data['Speed_kmh'][0] = 200.0
+    GPSData.data['Speed_kmh'][1] = 180.0
+    GPSData.data['Speed_kmh'][2] = 50.0
+    GPSData.data['Speed_kmh'][3] = 180.000000000001
+
+    SmoothData2 = GPSData.smoothData(GPSData.data)
+    SmoothData2Length = len(SmoothData2)
+
+    assert DataLength == SmoothData1Length
+    assert DataLength != SmoothData2Length
+    assert SmoothData2Length == 2
+    assert SmoothData2['Speed_kmh'][0] == 180.0
+    assert SmoothData2['Speed_kmh'][1] == 50.0
+
+    for i in SmoothData2['Speed_kmh']:
+        assert i <= 180.0
