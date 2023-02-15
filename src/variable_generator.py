5d0955582274fbf2c4edc575786130c7d32dccba
Clyde <jiangl21@mcmaster.ca>
2023-02-15 14:57:57 -0400

    update var_gen

--- src/variable_generator.py
+++ src/variable_generator.py
@@ -5,7 +5,7 @@ Creator: Mengtong Shi (shim17@mcmaster.ca)
 Requirements: Python 3.8 or later
 Date Created: Feb 05, 2023
 Last Revised: Feb 10, 2023
-Description: 
+Description:
 
 Version History:
 2023-02-05 (variable_generator.py)
@@ -36,39 +36,36 @@ def var_gen(route, network_edges):
 
     Parameters:
     route = A GeoDataframe that represents a route choice
-    network_graph = A Directed Graph object that is projected and
-                    contains the data of the transportation network
     network_edges = A Geodataframe that contains the data of
                     the edges in the Directed Graph
     """
 
-    serial_id = list(route['SerialID'].value_counts().index)
-
     # Project the route to the same CRS as the network dataset
-    # network_epsg = network_graph.graph['crs'].to_epsg()
-    # route_gdf_proj = route.to_crs(epsg=network_epsg)
     network_epsg = network_edges.crs.to_epsg()
     route_gdf_proj = route.to_crs(epsg=network_epsg)
-    RCA = []
-    distance = []
-    num_r = []
-    street_name = []
-    length = []
-
-
+    # Find geometry extract route segments with each geometry
     rca = list(route['geometry'].value_counts().index)
-    distance = []
-    num_lt = []
-    num_rt = []
-    num_r = []
-    street_name = []
-    length = []
+    # Find serial IDs extract route segments with each serial ID
+    serial_id = list(route['SerialID'].value_counts().index)
 
+    # The distance of the route
+    distance = []
+    # The number of left turns
+    num_lt = []
+    # The number of right turns
+    num_rt = []
+    # The dnumber of roads
+    num_r = []
+    # The longest street name
+    street_name = []
+    # The longest street name length
+    length = []
     for i in range(len(route_gdf_proj)):
         curr_route_coord = list(route_gdf_proj.loc[i]['geometry'].coords)
 
         # Get the length of the input route in meters
         route_dist_length = route_gdf_proj.loc[i]['geometry'].length
+        distance.append(route_dist_length)
         edges_route_passed = list(route_gdf_proj.loc[i]['edgesRoutePassed'])
         # print("Length of matched Route Choice is: " +
         #       str(round(route_dist_length, 2)) + " meters")
@@ -105,7 +102,7 @@ def var_gen(route, network_edges):
 def find_nearest_street(network_pe, edges_route_passed, coord):
     point_on_route = Point(coord[0], coord[1])
     nearest_dist = network_pe.loc[edges_route_passed[0]
-                                 ]['geometry'].distance(point_on_route)
+    ]['geometry'].distance(point_on_route)
     nearest_street = network_pe.loc[edges_route_passed[0]]['name']
     for edge_id in edges_route_passed[1:]:
         edge_geo = network_pe.loc[edge_id]['geometry']
@@ -122,23 +119,23 @@ def find_nearest_street(network_pe, edges_route_passed, coord):
 def count_turns(network_pe, edges_route_passed, route_coord):
     num_left_turn = 0
     num_right_turn = 0
-    for j in range(1, len(route_coord)-1):
-        line_segment1 = (route_coord[j], route_coord[j-1])
-        line_segment2 = (route_coord[j], route_coord[j+1])
+    for j in range(1, len(route_coord) - 1):
+        line_segment1 = (route_coord[j], route_coord[j - 1])
+        line_segment2 = (route_coord[j], route_coord[j + 1])
 
         # Using Dot Product to determine the angle between two line segments
         # Convert to vector form
-        vector1 = ((line_segment1[0][0]-line_segment1[1][0]),
-                   (line_segment1[0][1]-line_segment1[1][1]))
-        vector2 = ((line_segment2[0][0]-line_segment2[1][0]),
-                   (line_segment2[0][1]-line_segment2[1][1]))
+        vector1 = ((line_segment1[0][0] - line_segment1[1][0]),
+                   (line_segment1[0][1] - line_segment1[1][1]))
+        vector2 = ((line_segment2[0][0] - line_segment2[1][0]),
+                   (line_segment2[0][1] - line_segment2[1][1]))
         # Calculate dot product
-        dot_prod = vector1[0]*vector2[0] + vector1[1]*vector2[1]
+        dot_prod = vector1[0] * vector2[0] + vector1[1] * vector2[1]
         # Calculate magnitudes of two line segments
-        magnitude1 = (vector1[0]*vector1[0] + vector1[1]*vector1[1])**0.5
-        magnitude2 = (vector2[0]*vector2[0] + vector2[1]*vector2[1])**0.5
+        magnitude1 = (vector1[0] * vector1[0] + vector1[1] * vector1[1]) ** 0.5
+        magnitude2 = (vector2[0] * vector2[0] + vector2[1] * vector2[1]) ** 0.5
         # Calculate cosine value
-        cos_val = dot_prod/(magnitude1*magnitude2)
+        cos_val = dot_prod / (magnitude1 * magnitude2)
         # set cos_val to 1 or -1 if cos_val is not in the [-1,1] value range
         if cos_val > 1:
             cos_val = 1
@@ -149,28 +146,28 @@ def count_turns(network_pe, edges_route_passed, route_coord):
         angle_deg = math.degrees(angle_rad) % 360
 
         if angle_deg > 180:
-            angle_deg = 360-angle_deg
+            angle_deg = 360 - angle_deg
 
         # If an angle greater than 30 degrees is found,
         # Check if the two line segments of the angle are going from one street to another.
-        if abs(180-angle_deg) > 30:
+        if abs(180 - angle_deg) > 30:
             print(angle_deg)
             start_street = find_nearest_street(
-                network_pe, edges_route_passed, route_coord[j-1])
+                network_pe, edges_route_passed, route_coord[j - 1])
             print(start_street)
             end_street = find_nearest_street(
-                network_pe, edges_route_passed, route_coord[j+1])
+                network_pe, edges_route_passed, route_coord[j + 1])
             print(end_street)
             # If the two line segments of the angle are going from one street to another
             # Meaning a Turn is found. Then use cross product
             # to determine the direction of the turn(Left/Right)
             if start_street != end_street:
-                point_diff1 = (route_coord[j+1][0] - route_coord[j-1]
-                              [0], route_coord[j+1][1] - route_coord[j-1][1])
+                point_diff1 = (route_coord[j + 1][0] - route_coord[j - 1]
+                [0], route_coord[j + 1][1] - route_coord[j - 1][1])
                 point_diff2 = (
-                    route_coord[j][0] - route_coord[j-1][0], route_coord[j][1] - route_coord[j-1][1])
+                    route_coord[j][0] - route_coord[j - 1][0], route_coord[j][1] - route_coord[j - 1][1])
                 cross_prod = point_diff1[0] * point_diff2[1] - \
-                    point_diff2[0] * point_diff1[1]
+                             point_diff2[0] * point_diff1[1]
                 if cross_prod < 0:
                     print('Left Turn')
                     num_left_turn += 1
@@ -178,7 +175,7 @@ def count_turns(network_pe, edges_route_passed, route_coord):
                     print('Right Turn')
                     num_right_turn += 1
 
-    return {'left': num_left_turn, 'right': num_right_turn, 'total': (num_left_turn+num_right_turn)}
+    return {'left': num_left_turn, 'right': num_right_turn, 'total': (num_left_turn + num_right_turn)}
 
 
 def longest_leg(network_pe, edges_route_passed, route_coord):
@@ -188,7 +185,7 @@ def longest_leg(network_pe, edges_route_passed, route_coord):
     # lastPoint = route_coord[0]
     leg_len_dict[curr_street] = 0
     for i in range(1, len(route_coord)):
-        line_seg_len = Point(route_coord[i-1]).distance(Point(route_coord[i]))
+        line_seg_len = Point(route_coord[i - 1]).distance(Point(route_coord[i]))
         leg_len_dict[curr_street] += line_seg_len
         point_street = find_nearest_street(
             network_pe, edges_route_passed, route_coord[i])
@@ -212,4 +209,4 @@ def longest_leg(network_pe, edges_route_passed, route_coord):
             long_leg_len = curr_leg_len
         street_count += 1
 
-    return {'legStreet': long_leg_street, 'legLength': long_leg_len,'numOfStreets': street_count}
+    return {'legStreet': long_leg_street, 'legLength': long_leg_len, 'numOfStreets': street_count}
