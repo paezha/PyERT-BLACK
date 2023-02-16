1911f8e0f4e6232f609a24e18e98d95365a3b04e
ZabrainAli <48334272+ZabrainAli@users.noreply.github.com>
2023-02-15 20:46:40 -0500

    Update test_GPSPreprocess.py
    Changed test to snakecase
    
    Co-Authored-By: Mike Li <58126566+mikeli0623@users.noreply.github.com>
    Co-Authored-By: Jasper Leung <22846809+JasperLeungGit@users.noreply.github.com>
    Co-Authored-By: linqi Jiang <74118494+clydejiang@users.noreply.github.com>
    Co-Authored-By: HongzhaoTan <62959113+LilVegedog@users.noreply.github.com>
    Co-Authored-By: Mengtong Shi <48498366+vickysmt@users.noreply.github.com>

--- test/test_GPSPreprocess.py
+++ test/test_GPSPreprocess.py
@@ -11,72 +11,72 @@ sample_gps_file_path = os.getcwd().split("PyERT-BLACK")[0] + 'PyERT-BLACK/quarto
 @pytest.mark.parametrize(
     'sample_gps', [(sample_gps_file_path+'/sample-gps-1.csv')]
 )
-def test_getData(sample_gps):
-    Data = pd.read_csv(sample_gps)
-    DataLength = len(Data)
-    GPSData = gpsp.GPSPreprocess(data=Data)
-    ProcessedData = GPSData.getData()
-    ProcessedDataLength = len(ProcessedData)
-    point = gpd.points_from_xy(x=[ProcessedData['longitude'][0]], y=[ProcessedData['latitude'][0]])
-    assert ProcessedData['latitude'][0] == Data['latitude'][0]
-    assert ProcessedData['longitude'][0] == Data['longitude'][0]
-    assert ProcessedData['geometry'][0] == point[0]
-    assert 'geometry' not in Data
-    assert 'geometry' in ProcessedData
-    assert DataLength != ProcessedDataLength
+def test_get_data(sample_gps):
+    data = pd.read_csv(sample_gps)
+    data_length = len(data)
+    gps_data = gpsp.GPSPreprocess(data=data)
+    processed_data = gps_data.get_data()
+    processed_data_length = len(processed_data)
+    point = gpd.points_from_xy(x=[processed_data['longitude'][0]], y=[processed_data['latitude'][0]])
+    assert processed_data['latitude'][0] == data['latitude'][0]
+    assert processed_data['longitude'][0] == data['longitude'][0]
+    assert processed_data['geometry'][0] == point[0]
+    assert 'geometry' not in data
+    assert 'geometry' in processed_data
+    assert data_length != processed_data_length
 
 @pytest.mark.parametrize(
     'sample_gps', [(sample_gps_file_path+'/sample-gps-1.csv')]
 )
-def test_filterData(sample_gps):
-    Data = pd.read_csv(filepath_or_buffer=sample_gps, nrows=4)
-    Data1Length = len(Data)
-    GPSData = gpsp.GPSPreprocess(data=None)
-    GPSData.data = Data
-    FilteredData1 = GPSData.filterData(GPSData.data)
-    FilteredData1Length = len(FilteredData1)
-    Duplicate1 = FilteredData1.duplicated()
+def test_filter_data(sample_gps):
+    data = pd.read_csv(filepath_or_buffer=sample_gps, nrows=4)
+    data_length1 = len(data)
+    gps_data = gpsp.GPSPreprocess(data=None)
+    gps_data.data = data
+    filtered_data1 = gps_data.filter_data(gps_data.data)
+    filtered_data_length1 = len(filtered_data1)
+    duplicate1 = filtered_data1.duplicated()
 
-    GPSData.data = Data.append(Data[:1])
-    GPSData.data.reset_index(drop=True, inplace=True)
-    Data2Length = len(GPSData.data)
-    Duplicate2 = GPSData.data.duplicated()
-    FilteredData2 = GPSData.filterData(GPSData.data)
-    FilteredData2Length = len(FilteredData2)
-    Duplicate3 = FilteredData2.duplicated()
+    gps_data.data = data.append(data[:1])
+    gps_data.data.reset_index(drop=True, inplace=True)
+    data_length2 = len(gps_data.data)
+    duplicate2 = gps_data.data.duplicated()
+    filtered_data2 = gps_data.filter_data(gps_data.data)
+    filtered_data_length2 = len(filtered_data2)
+    duplicate3 = filtered_data2.duplicated()
 
-    assert Data1Length == FilteredData1Length
-    for i in Duplicate1:
+    assert data_length1 == filtered_data_length1
+    for i in duplicate1:
         assert i == False
-    assert Data2Length != FilteredData2Length
-    assert Duplicate2[4] == True
-    for i in Duplicate3:
+    assert data_length2 != filtered_data_length2
+    assert duplicate2[4] == True
+    for i in duplicate3:
         assert i == False
 
 @pytest.mark.parametrize(
     'sample_gps', [(sample_gps_file_path+'/sample-gps-1.csv')]
 )
-def test_smoothData(sample_gps):
-    Data = pd.read_csv(filepath_or_buffer=sample_gps, nrows=4)
-    DataLength = len(Data)
-    GPSData = gpsp.GPSPreprocess(data=None)
-    GPSData.data = Data
-    SmoothData1 = GPSData.smoothData(GPSData.data)
-    SmoothData1Length = len(SmoothData1)
+def test_smooth_data(sample_gps):
+    data = pd.read_csv(filepath_or_buffer=sample_gps, nrows=4)
+    data_length = len(data)
+    gps_data = gpsp.GPSPreprocess(data=None)
+    gps_data.data = data
+    smooth_data1 = gps_data.smoothdata(gps_data.data)
+    smooth_data_length1 = len(smooth_data1)
 
-    GPSData.data['Speed_kmh'][0] = 200.0
-    GPSData.data['Speed_kmh'][1] = 180.0
-    GPSData.data['Speed_kmh'][2] = 50.0
-    GPSData.data['Speed_kmh'][3] = 180.000000000001
+    gps_data.data['Speed_kmh'][0] = 200.0
+    gps_data.data['Speed_kmh'][1] = 180.0
+    gps_data.data['Speed_kmh'][2] = 50.0
+    gps_data.data['Speed_kmh'][3] = 180.000000000001
 
-    SmoothData2 = GPSData.smoothData(GPSData.data)
-    SmoothData2Length = len(SmoothData2)
+    smooth_data2 = gps_data.smooth_data(gps_data.data)
+    smooth_data_length2 = len(smooth_data2)
 
-    assert DataLength == SmoothData1Length
-    assert DataLength != SmoothData2Length
-    assert SmoothData2Length == 2
-    assert SmoothData2['Speed_kmh'][0] == 180.0
-    assert SmoothData2['Speed_kmh'][1] == 50.0
+    assert data_length == smooth_data_length1
+    assert data_length != smooth_data_length2
+    assert smooth_data_length2 == 2
+    assert smooth_data2['Speed_kmh'][0] == 180.0
+    assert smooth_data2['Speed_kmh'][1] == 50.0
 
-    for i in SmoothData2['Speed_kmh']:
+    for i in smooth_data2['Speed_kmh']:
         assert i <= 180.0
