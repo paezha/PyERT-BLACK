0363725d2e77bcaf66ecb7ee08da7567a5687aa8
HongzhaoTan <tanh10@mcmaster.ca>
2023-04-02 22:15:38 -0400

    Update unit tests for activity locations identification module due to major changes on the implementation of the module

--- test/test_activity_location.py
+++ test/test_activity_location.py
@@ -4,15 +4,18 @@ import pandas as pd
 import os
 import sys
 from shapely.geometry import Point, Polygon
-import activity_locations_identification as ali
 
-pal_data_from_gpkg = gpd.read_file("test_data/test_al_info_data.gpkg", layer='buildings')
-lu_data_from_gpkg = gpd.read_file("test_data/test_al_info_data.gpkg", layer='landuse')
+from src import activity_locations_identification as ali
+#import activity_locations_identification as ali
+
+test_data_path = os.getcwd().split("PyERT-BLACK")[0] + 'PyERT-BLACK/test/test_data'
+pal_data_from_gpkg = gpd.read_file(test_data_path + "/test_al_info_data.gpkg", layer='potential_activity_locations')
+lu_data_from_gpkg = gpd.read_file(test_data_path + "/test_al_info_data.gpkg", layer='landuse')
 pal_data_from_gpkg.set_index(['element_type', 'osmid'], inplace=True)
 lu_data_from_gpkg.set_index(['element_type', 'osmid'], inplace=True)
 pal_data_from_gpkg = pal_data_from_gpkg.to_crs(epsg=32620)
 lu_data_from_gpkg = lu_data_from_gpkg.to_crs(epsg=32620)
-test_stop_seg_path = "C:/Users/Clyde/PyERT-BLACK/src/test_data/test_al_info_stop_seg/test_al_info_stop_seg.shp"
+test_stop_seg_path = test_data_path + "/test_al_info_stop_seg/test_al_info_stop_seg.shp"
 test_stop_seg = gpd.read_file(test_stop_seg_path)
 test_stop_seg = test_stop_seg.to_crs(epsg=32620)
 
@@ -20,21 +23,31 @@ pal_gdf = pal_data_from_gpkg
 lu_gdf = lu_data_from_gpkg
 al_gdf = test_stop_seg
 
-
-def test_identify_lu():
-    test_lu_gdf = ali.identify_lu(al_gdf, lu_gdf)
-
+@pytest.mark.parametrize(
+    'al_gdf, lu_gdf, pal_gdf', 
+    [(al_gdf, lu_gdf, pal_gdf)]
+)
+def test_identify_lu(al_gdf, lu_gdf, pal_gdf):
+    test_lu_gdf = ali.create_al_info(al_gdf, lu_gdf, pal_gdf, 100)
     for i in range(test_lu_gdf.shape[0]):
-        assert test_lu_gdf.iloc[i]['lu_classification'] == 'residential'
+        #print(test_lu_gdf.iloc[i]['lu_type'])
+        assert test_lu_gdf.iloc[i]['lu_type'] == 'residential'
         assert test_lu_gdf.iloc[i]['lu_index'] == ('relation', 13745200)
 
-
-def test_identify_pal():
-    test_pal_gdf = ali.identify_pal(al_gdf, pal_gdf)
+@pytest.mark.parametrize(
+    'al_gdf, lu_gdf, pal_gdf', 
+    [(al_gdf, lu_gdf, pal_gdf)]
+)
+def test_identify_pal(al_gdf, lu_gdf, pal_gdf):
+    test_pal_gdf = ali.create_al_info(al_gdf, lu_gdf, pal_gdf, 100)
 
     for i in range(test_pal_gdf.shape[0]):
-        point = al_gdf.iloc[i]['geometry']
-
-        polygon = test_pal_gdf.iloc[i]['building_geometry']
-        distance = point.distance(polygon)
-        assert distance < 100
+        polygon = test_pal_gdf.iloc[i]['geometry']
+        point =  al_gdf.loc[0]['geometry']
+        min_dist = point.distance(polygon)
+        for point in list(al_gdf['geometry']):
+            curr_dist = point.distance(polygon)
+            if min_dist > curr_dist:
+                min_dist = curr_dist
+        #print(min_dist)
+        assert min_dist < 100
